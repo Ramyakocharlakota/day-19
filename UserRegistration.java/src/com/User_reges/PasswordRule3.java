8697323ef431a3eea97a036163a7dc3ed021d67f
Ramya kocharlakota <Ramyakocharlakota@gmail.com>
2022-01-07 14:56:30 +0530

    Adding pswd should have atleast 1 numeric value

--- UserRegistration.java/src/com/User_reges/PasswordRule3.java
+++ UserRegistration.java/src/com/User_reges/PasswordRule3.java
@@ -1,5 +1,28 @@
 package com.User_reges;
 
-public class PasswordRule3 {
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class PasswordRule3 {
+	public boolean passwrdRule(String inputStr) {
+		String passwordPattern = "^(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{8,}$";
+		Pattern regex = Pattern.compile(passwordPattern);
+		Matcher passwordMatcher = regex.matcher(inputStr);
+		if (passwordMatcher.matches()) {
+			System.out.println(inputStr + " succesfully loggedin and passed third rule.");
+		} else {
+			System.out.println(inputStr + " is invalid password.Kindly input minimum 8 character.");
+		}
+		return false;
+	}
+	public static void main (String[] args) {
+		PasswordRule3 psswrd = new PasswordRule3();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please log in using 8 character password having one upper case and one numeric number.");
+		System.out.print("Enter the password : ");
+		String inputStr = sc.nextLine();
+		sc.close();
+		psswrd.passwrdRule(inputStr);
+	}
 }
